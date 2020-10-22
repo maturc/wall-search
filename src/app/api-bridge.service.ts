e36c6009a169d5e8bc163f6f767cbe57438d9163
Matija Turčec <turcec.matija@gmail.com>
2020-10-22 12:53:15 +0200

    changed main fetch method

--- src/app/api-bridge.service.ts
+++ src/app/api-bridge.service.ts
@@ -9,7 +9,7 @@ export class ApiBridgeService {
   //https://wallhaven.cc/help/api
   url: string = "/api/v1/search?apikey=V2rSnX71qCvSyWFtXuABi4WU7bimA6SP&categories=100";
   constructor( private http: HttpClient ) {}
-  getLatest() {
-    return this.http.get(this.url);
+  getLatest( pageNumber: number = 1 ) {
+    return this.http.get(this.url + `&page=${pageNumber}`);
   }
 }
