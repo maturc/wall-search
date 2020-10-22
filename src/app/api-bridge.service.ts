12f204498d6399a330d3642a68970e38a1fd3c0a
Matija Turčec <turcec.matija@gmail.com>
2020-10-22 23:20:08 +0200

    final

--- src/app/api-bridge.service.ts
+++ src/app/api-bridge.service.ts
@@ -7,7 +7,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class ApiBridgeService {
   //api docs
   //https://wallhaven.cc/help/api
-  url: string = "/api/v1/search?apikey=V2rSnX71qCvSyWFtXuABi4WU7bimA6SP&categories=100";
+  url: string = "/api/v1/search?categories=100";
   constructor( private http: HttpClient ) {}
   getLatest( pageNumber: number = 1 ) {
     return this.http.get(this.url + `&page=${pageNumber}`);
